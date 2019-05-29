13d5dde130f9505dec5b4524921c1897167986ba
Will Adams <bushblade@gmail.com>
2019-05-29 21:27:13 +0100

    logo and animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Logo from '../components/logo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Logo />
   </Layout>
 )
 
