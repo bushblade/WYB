a4043b99846285b09699c5666b77cb37bf9a49d1
Will Adams <bushblade@gmail.com>
2020-01-14 16:11:44 +0000

    basic side menu

---
+++ src/components/sideMenu/MenuButton.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Button = styled.div`
+  position: fixed;
+  top: 1rem;
+  left: 1.5rem;
+  z-index: 12;
+  display: block;
+  width: 2.5rem;
+  height: 2.5rem;
+  cursor: pointer;
+  margin: 0 auto;
+  transition: all 0.3s;
+  span {
+    position: absolute;
+    top: 50%;
+    display: block;
+    width: 100%;
+    height: 0.3rem;
+    background-color: rgb(51, 51, 51);
+    border-radius: 3px;
+  }
+  span:before,
+  span:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(51, 51, 51);
+    border-radius: 3px;
+    transition: all 0.3s;
+  }
+  span:before {
+    transform: translateY(-0.7rem);
+  }
+  span:after {
+    transform: translateY(0.7rem);
+  }
+  ${props =>
+    props.open &&
+    css`
+      transform: rotate(45deg);
+      span {
+        background-color: whitesmoke;
+      }
+      span:before {
+        transform: rotate(90deg);
+        background-color: whitesmoke;
+      }
+      span:after {
+        transform: rotate(90deg);
+        background-color: whitesmoke;
+      }
+    `}
+  @media (min-width: 1000px) {
+    display: none;
+  }
+`
+
+const MenuButton = ({ open, setOpen }) => (
+  <Button open={open} onClick={() => setOpen(!open)}>
+    <span></span>
+  </Button>
+)
+export default MenuButton
--- src/components/sideMenu/index.js
+++ src/components/sideMenu/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { colours } from '../styled'
+import { animated, useSpring } from 'react-spring'
 
-const SideMenu = ({ className }) => {
-  return <nav className={className}></nav>
+import { colours } from '../styled'
+import MenuButton from './MenuButton'
+
+const Menu = styled.nav`
+  height: 100vh;
+  background-color: ${colours.darkGrey};
+  width: 50%;
+  z-index: 10;
+  @media (max-width: 1000px) {
+    position: fixed;
+    transition: transform 0.3s ease-in-out;
+    transform: ${props =>
+      props.open ? 'translate3d(0, 0, 0)' : 'translate3d(-100%, 0, 0)'};
+  }
+  @media (min-width: 1000px) {
+    width: 100%;
+    grid-area: menu;
+  }
+`
+
+const SideMenu = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <MenuButton setOpen={setOpen} open={open} />
+      <Menu open={open}></Menu>
+    </>
+  )
 }
 
-export default styled(SideMenu)`
-  width: 100%;
-  height: 100vh;
-  grid-area: menu;
-  background-color: ${colours.darkGrey};
-`
+export default SideMenu
