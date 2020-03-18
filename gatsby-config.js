07d0b911e5f33f3282c488ed47c7270232c5bd2a
Will Adams <bushblade@gmail.com>
2020-03-18 14:21:09 +0000

    plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     `gatsby-plugin-react-helmet-async`,
     {
       resolve: `gatsby-source-filesystem`,
