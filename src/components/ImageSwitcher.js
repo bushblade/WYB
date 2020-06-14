8942c29dd05ee2bfb399e2cc2f1ebc82f3bdc103
Will Adams <bushblade@gmail.com>
2020-06-14 16:12:46 +0100

    almost there

--- src/components/ImageSwitcher.js
+++ src/components/ImageSwitcher.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useTransition, animated, config } from 'react-spring'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import styled from 'styled-components'
 
 const ImageContainer = styled.div`
-  height: 100%;
   display: flex;
   align-items: center;
+  height: 100%;
+  width: 100%;
   justify-content: flex-end;
   .gatsby-image-wrapper {
     min-width: 300px;
     width: 100%;
-    max-height: 86vh;
   }
 `
 const query = graphql`
@@ -41,6 +41,7 @@ const ImageSwitcher = props => {
   const images = useStaticQuery(query).allFile.edges.map(({ node }) => node)
 
   const [index, setIndex] = useState(0)
+  const [imageHeight, setImageHeight] = useState(100)
 
   const transitions = useTransition(
     images[index],
@@ -53,11 +54,12 @@ const ImageSwitcher = props => {
     }
   )
 
+  const imageRef = useRef()
+
   useEffect(() => {
     let interval = setInterval(
       () =>
         setIndex(state => {
-          console.log('setting index')
           const next = state + 1
           return next === images.length ? 0 : next
         }),
@@ -66,8 +68,15 @@ const ImageSwitcher = props => {
     return () => clearInterval(interval)
   }, [images.length])
 
+  useEffect(() => {
+    if (imageRef.current) {
+      console.log(imageRef.current.clientHeight)
+      setImageHeight(imageRef.current.clientHeight)
+    }
+  })
+
   return (
-    <div style={{ position: 'relative' }}>
+    <div style={{ position: 'relative', height: imageHeight }}>
       {transitions.map(({ item, props, key }) => (
         <animated.div
           key={key}
@@ -80,7 +89,7 @@ const ImageSwitcher = props => {
             height: '100%',
           }}
         >
-          <ImageContainer>
+          <ImageContainer ref={imageRef}>
             <Img fluid={item.childImageSharp.fluid} />
           </ImageContainer>
         </animated.div>
