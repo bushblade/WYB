9bc94b5e0c25c7f49b86e5f60fa732ca35383a0d
Will Adams <bushblade@gmail.com>
2020-08-01 17:16:05 +0100

    contact heading

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { TwoColumnContainer, Cell } from '../components/styled'
 import ContactForm from '../components/contactform'
+import styled from 'styled-components'
 import Poem from '../components/Poem'
 
 const Contact = ({ className }) => {
   return (
     <div className={className}>
+      <header>
+        <h2>Contact West Yorkshire Bushcraft</h2>
+        <p>
+          For questions, comments and membership enquiries please get in
+          touch...
+        </p>
+      </header>
       <TwoColumnContainer>
         <Cell left>
           <ContactForm />
@@ -18,4 +26,13 @@ const Contact = ({ className }) => {
   )
 }
 
-export default Contact
+export default styled(Contact)`
+  header {
+    width: 100%;
+    height: 100%;
+    padding: 0 2rem 1rem 2rem;
+    @media (min-width: 1000px) {
+      padding: 0 3rem 1rem 3rem;
+    }
+  }
+`
